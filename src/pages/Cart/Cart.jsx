cd61251d21a9f4b00fb715637e4ce9e3b85d6b1c
erronintila <erronintila@gmail.com>
2022-06-29 05:28:31 +0800

    Setup cart layout

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,7 +1,54 @@
 import React from 'react'
+import jsonData from '../../products.json'
+import style from './Cart.module.css'
 
 function Cart() {
-  return <div>Cart Page</div>
+  const cartItems = jsonData.cartItems
+  // const cartItems = []
+  return (
+    <div className='container'>
+      <h1>Cart</h1>
+      <div className={style.tableContainer}>
+        {cartItems && cartItems.length ? (
+          <div className={style.wrapper}>
+            <table>
+              <thead>
+                <tr>
+                  <th>Product Name</th>
+                  <th>Unit Price</th>
+                  <th>Quantity</th>
+                  <th>Total</th>
+                </tr>
+              </thead>
+              <tbody>
+                {cartItems.map((item, index) => {
+                  return (
+                    <tr key={index}>
+                      <td>{item.name}</td>
+                      <td>{item.price}</td>
+                      <td>{item.quantity}</td>
+                      <td>
+                        {Number(item.quantity ?? 0) * Number(item.price ?? 0)}
+                      </td>
+                    </tr>
+                  )
+                })}
+              </tbody>
+              <tfoot>
+                <tr>
+                  <td colSpan={2}></td>
+                  <td>Total</td>
+                  <td>0</td>
+                </tr>
+              </tfoot>
+            </table>
+          </div>
+        ) : (
+          <div>No items in the cart.</div>
+        )}
+      </div>
+    </div>
+  )
 }
 
 export default Cart
